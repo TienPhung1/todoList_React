5901b1aff1c5b0a2abfd4d7a75fd0a0cfd6f463b
tien <101317958+TienPhung1@users.noreply.github.com>
2023-05-16 16:21:23 +0700

    modaladdnew

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
+import Container from "react-bootstrap/Container";
+import ModalAddNew from "./components/ModalAddNew";
+import { useState } from "react";
 
 function App() {
+  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
+
+  const handleClose = () => {
+    setIsShowModalAddNew(false);
+  };
   return (
     <div className="app-container">
       <Header />
-      <TableUsers />
+
+      <Container>
+        <div className="my-3 add-new">
+          <span>
+            <b>list Users:</b>
+          </span>
+          <button
+            class="btn btn-primary"
+            onClick={() => setIsShowModalAddNew(true)}
+          >
+            Add New Todo
+          </button>
+        </div>
+        <TableUsers />
+      </Container>
+
+      <ModalAddNew show={isShowModalAddNew} handleClose={handleClose} />
     </div>
   );
 }
---
+++ src/components/ModalAddNew.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+const ModalAddNew = ({ show, handleClose }) => {
+  const [name, setName] = useState("");
+  const [job, setJob] = useState("");
+
+  const handleSaveUser = () => {};
+  return (
+    <div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="body-add-name">
+            <form>
+              <div className="mb-3">
+                <label className="form-label">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Job</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={job}
+                  onChange={(e) => setJob(e.target.value)}
+                />
+              </div>
+            </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => handleSaveUser()}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalAddNew;
