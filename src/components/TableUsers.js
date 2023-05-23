c63fac2dc4563058c66757f0a457d4bc7c3244d2
tien <101317958+TienPhung1@users.noreply.github.com>
2023-05-23 10:45:25 +0700

    import csv

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -8,7 +8,9 @@ import ReactPaginate from "react-paginate";
 import ModalEditUser from "./ModalEditUser";
 import ModalConfirm from "./ModalConfirm";
 import { CSVLink, CSVDownload } from "react-csv";
+import Papa from "papaparse";
 import "./TableUser.scss";
+import { toast } from "react-toastify";
 
 const TableUsers = () => {
   const [listUsers, setListUsers] = useState([]);
@@ -113,6 +115,50 @@ const TableUsers = () => {
       done();
     }
   };
+
+  const handleImportCSV = (e) => {
+    if (e.target && e.target.files && e.target.files[0]) {
+      let file = e.target.files[0];
+      if (file.type !== "text/csv") {
+        toast.error("Only Accept CSV...");
+        return;
+      }
+      Papa.parse(file, {
+        // header: true,
+        complete: function (results) {
+          let rawCSV = results.data;
+          if (rawCSV.length > 0) {
+            if (rawCSV[0] && rawCSV[0].length === 3) {
+              if (
+                rawCSV[0][0] !== "email" ||
+                rawCSV[0][1] !== "first_name" ||
+                rawCSV[0][2] !== "last_name"
+              ) {
+                toast.error("Wrong format CSV file");
+              }
+            } else {
+              let result = [];
+
+              rawCSV.map((item, index) => {
+                if (index > 0 && item.length === 3) {
+                  let obj = {};
+                  obj.email = item[0];
+                  obj.firstName = item[1];
+                  obj.lastName = item[2];
+                  result.push(obj);
+                }
+              });
+              setListUsers(result);
+            }
+          } else {
+            toast.error("Not found data on CSV file");
+          }
+          console.log(results.data);
+        },
+      });
+    }
+  };
+
   return (
     <div>
       <div className="my-3 add-new">
@@ -124,7 +170,12 @@ const TableUsers = () => {
           <label htmlFor="\" className="btn btn-primary">
             Import
           </label>
-          <input id="test" type="file" hidden />
+          <input
+            id="test"
+            type="file"
+            onChange={(e) => handleImportCSV(e)}
+            hidden
+          />
 
           <CSVLink
             data={dataExport}
